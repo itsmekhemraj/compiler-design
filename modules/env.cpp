bdde087418881642e25e469cb1085f467c5a304c
Niyoj Oli <oliniyoj@gmail.com>
2022-10-26 14:31:52 +0545

    feat: adds compiler handler cpp

---
+++ modules/env.cpp
@@ -0,0 +1,21 @@
+// class `CompilerEnvironemnt` is responsible to take the argumens from terminal from user and set up the environment from the compiler
+class CompilerEnvironment {
+    public:
+        bool is_readable;
+        bool is_lexer_only;
+
+        CompilerEnvironment() {
+            this->is_readable = false;
+            this->is_lexer_only = false; 
+        }
+
+        void set_info(int argc, char*argv[]);
+};
+
+// function `set_info()` is used to interact with the data members of `CompilerEnvironment`
+void CompilerEnvironment::set_info(int argc, char*argv[]) {
+    for(int i=0; i<argc; i++) {
+        if (std::string(argv[i]) == "--lexer-only") this->is_lexer_only = true;
+        if (std::string(argv[i]) == "--readable") this->is_readable = true;
+    }
+}
